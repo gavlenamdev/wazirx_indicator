bcbd4509a8cf395ed7ba03864655f594c703ee86
Namdev Gavle <gavlenamdevprabha@gmail.com>
2021-05-14 22:52:35 +0530

    configs updated

--- config.py
+++ config.py
@@ -7,17 +7,7 @@ REFRESH_TIME = 4000
 
 STOCKS = {
     "doge": {"currency": "INR", "current_price": "..."},
-    "wrx": {"currency": "INR", "current_price": "..."},
-    "shib": {"currency": "INR", "current_price": "..."},
-    "trx": {"currency": "INR", "current_price": "..."},
-    "zrx": {"currency": "INR", "current_price": "..."},
-    "vet": {"currency": "INR", "current_price": "..."},
-    "hbar": {"currency": "INR", "current_price": "..."},
-    "matic": {"currency": "INR", "current_price": "..."},
-    "ada": {"currency": "INR", "current_price": "..."},
-    "ftm": {"currency": "INR", "current_price": "..."},
-    "eth": {"currency": "INR", "current_price": "..."},
-    "sc": {"currency": "INR", "current_price": "..."}
+    "wrx": {"currency": "INR", "current_price": "..."}
 }
 
 PRIMARY_STOCK = "doge"
