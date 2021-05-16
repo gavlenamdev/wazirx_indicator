275bc9fe5ea60d9b06d70dda6471f2f1c1213e6b
Namdev Gavle <gavlenamdevprabha@gmail.com>
2021-05-16 22:26:23 +0530

    change method moved to class

--- wazirx_indicator/__init__.py
+++ wazirx_indicator/__init__.py
@@ -10,36 +10,25 @@ from config import APPLICATION_IDENTIFIER, ICON_PATH, REFRESH_TIME, STOCKS, PRIM
 from wazirx_indicator.helpers import get_stock_value, fetch_current_market
 
 
-def change_label(indicator, stocks, widgets, primary):
-    data = fetch_current_market()
-    # print(data)
-    indicator.set_label(str(get_stock_value(data, coin=primary, include_high_low=False)), '')
-    for widget in widgets:
-        stock_name = widget.get_label().lower().split(":")[0]
-        stock_data = get_stock_value(data, coin=stock_name)
-        widget.set_label(stock_data)
-    return True
-
-
 class MyApplication(Gtk.Application):
 
     def __init__(self):
         Gtk.Application.__init__(self)
+        self.menu_item_list = []
+        self.indicator = appindicator.Indicator.new(APPLICATION_IDENTIFIER, ICON_PATH,
+                                                    appindicator.IndicatorCategory.SYSTEM_SERVICES)
+        self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
     def do_activate(self):
-        indicator = appindicator.Indicator.new(APPLICATION_IDENTIFIER, ICON_PATH,
-                                               appindicator.IndicatorCategory.SYSTEM_SERVICES)
-        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
         # create a menu
         menu = Gtk.Menu()
 
-        menu_item_list = []
         for stock in STOCKS.keys():
             menu_item = Gtk.MenuItem().new_with_label(label=stock.upper())
             menu.append(menu_item)
             menu_item.show_all()
-            menu_item_list.append(menu_item)
+            self.menu_item_list.append(menu_item)
 
         separator = Gtk.SeparatorMenuItem()
         menu.append(separator)
@@ -50,8 +39,8 @@ class MyApplication(Gtk.Application):
         menu_item.show_all()
 
         # add to indicator
-        indicator.set_menu(menu)
-        GLib.timeout_add(REFRESH_TIME, change_label, indicator, STOCKS, menu_item_list, PRIMARY_STOCK)
+        self.indicator.set_menu(menu)
+        GLib.timeout_add(REFRESH_TIME, self.change_label)
         Gtk.main()
 
     def quit(self, source):
@@ -59,3 +48,27 @@ class MyApplication(Gtk.Application):
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
+
+    def change_label(self):
+        data = fetch_current_market()
+        # print(data)
+        self.indicator.set_label(str(get_stock_value(data, coin=PRIMARY_STOCK, include_high_low=False)), '')
+        has_error = False
+        for menu_item in self.menu_item_list:
+            stock_name = menu_item.get_label().lower().split(":")[0]
+            print(stock_name)
+            print(stock_name == "")
+            if stock_name != "":
+                stock_label = get_stock_value(data, coin=stock_name)
+                menu_item.set_label(stock_label)
+            else:
+                # stock name is empty means, may be some network error. So break
+                has_error = True
+                break
+        if not has_error:
+            return True
+
+        for stock, menu_item in zip(STOCKS.keys(), self.menu_item_list):
+            menu_item.set_label(stock.upper())
+            menu_item.show_all()
+        return True
