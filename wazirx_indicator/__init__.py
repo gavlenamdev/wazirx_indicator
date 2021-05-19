f2c2eb38ffe1c87972cccd7bdcd6bd9833c65449
Namdev Gavle <gavlenamdevprabha@gmail.com>
2021-05-19 15:10:37 +0530

    click on coin to make it primary

--- wazirx_indicator/__init__.py
+++ wazirx_indicator/__init__.py
@@ -3,7 +3,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk, GLib, Gdk
 from gi.repository import AppIndicator3 as appindicator
 
 from config import APPLICATION_IDENTIFIER, ICON_PATH, REFRESH_TIME, STOCKS, PRIMARY_STOCK
@@ -15,6 +15,7 @@ class MyApplication(Gtk.Application):
     def __init__(self):
         Gtk.Application.__init__(self)
         self.menu_item_list = []
+        self.primary_stock = PRIMARY_STOCK
         self.indicator = appindicator.Indicator.new(APPLICATION_IDENTIFIER, ICON_PATH,
                                                     appindicator.IndicatorCategory.SYSTEM_SERVICES)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
@@ -26,6 +27,7 @@ class MyApplication(Gtk.Application):
 
         for stock in STOCKS.keys():
             menu_item = Gtk.MenuItem().new_with_label(label=stock.upper())
+            menu_item.connect("activate", self.set_as_primary)
             menu.append(menu_item)
             menu_item.show_all()
             self.menu_item_list.append(menu_item)
@@ -51,7 +53,7 @@ class MyApplication(Gtk.Application):
 
     def change_label(self):
         data = fetch_current_market()
-        prime_stock_price = get_stock_value(data, coin=PRIMARY_STOCK, include_high_low=False)
+        prime_stock_price = get_stock_value(data, coin=self.primary_stock, include_high_low=False)
         self.indicator.set_label(prime_stock_price, '')
         has_error = False
         for menu_item in self.menu_item_list:
@@ -61,6 +63,7 @@ class MyApplication(Gtk.Application):
             if stock_name != "":
                 stock_label = get_stock_value(data, coin=stock_name)
                 menu_item.set_label(stock_label)
+
             else:
                 # stock name is empty means, may be some network error. So break
                 has_error = True
@@ -71,7 +74,13 @@ class MyApplication(Gtk.Application):
         temp_menu_item_list = []
         for stock, menu_item in zip(STOCKS.keys(), self.menu_item_list):
             menu_item.set_label(stock.upper())
+            # menu_item.connect("activate", self.set_as_primary)
             menu_item.show_all()
             temp_menu_item_list.append(menu_item)
         self.menu_item_list = temp_menu_item_list
         return True
+
+    def set_as_primary(self, menu_item):
+        stock_name = menu_item.get_label().lower().split(":")[0]
+        print(stock_name)
+        self.primary_stock = stock_name
